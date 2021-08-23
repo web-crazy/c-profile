f5461171e31b5a773bc6e2dfbe34a15201d2653b
webcrazy <webcrazy1987@gmail.com>
2021-08-23 03:58:32 -0500

    feat: resume

---
+++ contact_form/contact_form.php
@@ -0,0 +1,10 @@
+<?php
+
+// configure
+$from = 'Contact form <yourmail@domain.com>';
+$sendTo = 'your@mail.com';
+$subject = 'New message from contact form';
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
